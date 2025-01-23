515c7322c380b231f6f2cb54622e8a9db8b8dca0
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2025-01-23 18:47:12 -0300

    Implementation Delete Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,9 +15,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         // exemplo usando a classe FactoryDao para criar os Daos sem usar um (new etc etc etc)
         SellerDao sellerDao = DaoFactory.createSellerDao();
@@ -37,25 +40,29 @@ public class Program {
 
         System.out.println("\n=== Teste 3: Seller findAll ===");
         list = sellerDao.findAll();
-
         for (Seller s : list) {// Percorre a lista de vendedores retornada e imprime as informações de cada vendedor
             // Imprime as informações do vendedor
             System.out.println(s);
         }
-
 
         System.out.println("\n=== Teste 4: Seller Insert  ===");
         Seller newSeller = new Seller("Isaac Drumond Reis",null,90000.0,"isaacdrumond@",new Date(),department);
         sellerDao.insert(newSeller);
         System.out.println("Inserido com sucesso! ID: " + newSeller.getId());
 
-
         System.out.println("\n === Teste 5: Seller Update  ===");
         seller = sellerDao.findById(3);
-        seller.setName("Alex Grey Reis");
+        seller.setId(11);
         sellerDao.update(seller);
         System.out.println("Atualizado com sucesso! ID: " + seller.getId());
 
+        System.out.println("\n === Teste 6: Delete Seller === ");
+        System.out.println("Digite o ID que deseja excluir: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Deletado com sucesso! ID: " + id);
 
+
+        sc.close();
     }
 }
