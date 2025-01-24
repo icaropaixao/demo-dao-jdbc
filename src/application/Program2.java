5e38b76531bc314a5eb25bab12c98475e02c7f33
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2025-01-24 16:43:33 -0300

    Create DeleteDepartment

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,18 +4,26 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.Scanner;
+
 public class Program2 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         // exemplo usando a classe actoryDao para criar os Daos sem usar um (new etc etc etc)
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
         System.out.println("=== Teste 1: Insert Department === ");
-        Department dep = new Department(6,"Usinagem");
+        Department dep = new Department(6, "Usinagem");
         departmentDao.insert(dep);
         System.out.println("Inserido com sucesso! ID: " + dep.getId());
 
         System.out.println("=== Teste 2: Delete Department === ");
+        System.out.println("Digite o ID que deseja excluir: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Deletado com sucesso! ID: " + id);
 
 
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -58,14 +58,34 @@ public class DepartmentDaoJDBC implements DepartmentDao {
     }
 
     @Override
+    public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            // codigo SQL
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                       + "WHERE Id = ? ");
+
+            st.setInt(1,id);
+
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
+    }
+
+
+    @Override
     public void update(Department obj) {
 
     }
 
-    @Override
-    public void deleteById(Integer id) {
-
-    }
 
     @Override
     public Department findById(Integer id) {
